5a33f3540e67550291200576b94d5d5a3a43cdab
David DeTomaso <davedeto@gmail.com>
2015-02-25 00:38:30 -0800

    Release 0.7.2
    Signed-off-by: David DeTomaso <davedeto@gmail.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "FastProject",
-	version = "0.7.1",
+	version = "0.7.2",
 	packages = find_packages(),
 	
 	install_requires = ['numpy>=1.9','matplotlib>=1.4.0','scikit-learn>=0.15.2'],
