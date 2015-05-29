c3aa8d684e230a74d8023131ae18f785bff8691d
David DeTomaso <davedeto@gmail.com>
2015-05-28 17:02:40 -0700

    Version 0.7.4
    Signed-off-by: David DeTomaso <davedeto@gmail.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "FastProject",
-	version = "0.7.3",
+	version = "0.7.4",
 	packages = find_packages(),
 
 	entry_points = { 'console_scripts': ['FastProject = FastProject.__main__:entry']},
