9d87cbe9a43902c6625df885c5b24fa9e4bc3131
David DeTomaso <davedeto@gmail.com>
2015-06-12 19:04:22 -0700

    Better optimized em method.
    Signed-off-by: David DeTomaso <davedeto@gmail.com>

--- FastProject/Utils/em.py
+++ FastProject/Utils/em.py
@@ -5,97 +5,85 @@ Created on Sun Dec 14 12:29:19 2014
 @author: daved_000
 """
 
-from numpy import *
+import numpy as np;
 from FastProject.Utils import ProgressBar
 
 #zmat is Genes x Samples
 #cutoff is Genes x 1 - represents the initial cutoff between normal and exponential distributions
 
 def em_exp_norm_mixture(zmat, cutoff, progressbar = True):
-    with errstate(divide='ignore', invalid='ignore'):  #nans and infs are handled explicitly, don't need warning
+    with np.errstate(divide='ignore', invalid='ignore'):  #nans and infs are handled explicitly, don't need warning
         max_iter = 150;
-    
+
         if(progressbar): pbar = ProgressBar(max_iter);
-        
+
         #promote to 2d if single gene given
         if(zmat.ndim == 1):
-            zmat = zmat.reshape(1, zmat.shape[0]);
-            cutoff = array([cutoff]);
-        
-        cutoff = cutoff.reshape(cutoff.shape[0],1); 
-        cutoffs = tile(cutoff, (1,zmat.shape[1]));
+            zmat = zmat.reshape((1, zmat.shape[0]));
+            cutoff = np.array([cutoff]);
+
+        cutoff = cutoff.reshape((cutoff.shape[0],1));
+        cutoffs = np.tile(cutoff, (1,zmat.shape[1]));
         gamma = zmat > cutoffs;
-        Pi = mean(gamma,1).reshape((zmat.shape[0],1));
-        
+        Pi = np.mean(gamma,axis=1).reshape((zmat.shape[0],1));
+
         mu_l=weighted_mean(zmat,1-gamma);
         mu_h=weighted_mean(zmat,gamma);
-        st_h=weighted_std(zmat,gamma);
-        st_l=weighted_std(zmat,1-gamma);
-        st_l[~isfinite(st_l)]=1e-3;
-        
-        to_stop = zeros((zmat.shape[0],1)) > 1;
-        
-        p_low = zeros(zmat.shape);
-        p_high = zeros(zmat.shape);    
-        
-        for niter in arange(0,max_iter)+1:
-            gamma_prev = gamma;
+        st_h=weighted_std(zmat,gamma, mu_h);
+
+        for niter in xrange(max_iter):
             #E
-            mu_l2 = tile(mu_l, (1,zmat.shape[1]));
-            p_low = exp(-1*zmat/mu_l2)/mu_l2;
-            
-            mu_h2 = tile(mu_h, (1,zmat.shape[1]));
-            st_h2 = tile(st_h, (1,zmat.shape[1]));
-            p_high = exp(-1 * (zmat - mu_h2)**2 / (2*st_h2**2)) / st_h2 / sqrt(2*pi);
-            
-            p_low[~isfinite(p_low)] = 1e-5;
+            prev_gamma = gamma;
+            p_low = np.exp(-1*zmat/mu_l)/mu_l;
+
+            p_high = np.exp(-1 * (zmat - mu_h)**2 / (2*st_h**2)) / st_h / np.sqrt(2*np.pi);
+
+            p_low[~np.isfinite(p_low)] = 1e-5;
             p_low[p_low < 1e-5] = 1e-5;
-            
-            p_high[~isfinite(p_high)] = 1e-5;
+
+            p_high[~np.isfinite(p_high)] = 1e-5;
             p_high[p_high<1e-5]   = 1e-5;
-            
-            Pix = tile(Pi,(1,zmat.shape[1]));
-            
-            gamma = (Pix * p_high) / ( ((1-Pix)*p_low) + (Pix*p_high));
-            #gamma[to_stop] = gamma_prev[to_stop]; #positions marked (done) are frozen
-    
+
+            gamma = (Pi * p_high) / ( ((1-Pi)*p_low) + (Pi*p_high));
+
             #M
-            Pi = mean(gamma,1).reshape((zmat.shape[0],1));
+            Pi = np.mean(gamma,axis=1).reshape((zmat.shape[0],1));
             mu_l=weighted_mean(zmat,1-gamma);
             mu_h=weighted_mean(zmat,gamma);
-            st_h=weighted_std(zmat,gamma);
-            st_l=weighted_std(zmat,1-gamma);
-            st_l[~isfinite(st_l)]=1e-3;
-            
-            L = sum(gamma * log(p_high) + (1-gamma)*log(p_low),axis=1);
-            
-            #Stop
-            to_stop = (gamma_prev > 0.5) == (gamma > 0.5);
-            d=sum(to_stop) / float(size(to_stop));
-            
+            st_h=weighted_std(zmat,gamma, mu_h);
+
+
             if(progressbar): pbar.update();
-            
-            #if niter == 1: print mu_l, mu_h, st_l, st_h    
+
+            if(niter % 10 == 0):
+                biggest_change = np.max(np.abs(gamma - prev_gamma));
+                if(biggest_change < 0.01):
+                    break;
+
+
+            #if niter == 1: print mu_l, mu_h, st_l, st_h
             #print 'Iteration: ', niter, ' L: ', sum(L);
             #if d>0.95:
-                #break;
+            #break;
         if(progressbar): pbar.complete();
-        
-        return (gamma, mu_l, mu_h, st_l, st_h, Pi, L);
 
+        L = np.sum(gamma * np.log(p_high) + (1-gamma)*np.log(p_low),axis=1);
+        st_l=weighted_std(zmat,1-gamma, mu_l);
+        return (gamma, mu_l, mu_h, st_l, st_h, Pi, L);
 
 #y and gamma same size
 #returns average of each row, weighted by gammas
 def weighted_mean(y, gamma):
-    mu = sum(gamma * y, axis=1) / sum(gamma, axis=1);
-    mu = mu.reshape(mu.shape[0], 1);  #make it 2 dimensional    
+    mu = np.sum(gamma * y, axis=1) / np.sum(gamma, axis=1);
+    mu = mu.reshape((mu.shape[0], 1));  #make it 2 dimensional
     return mu
 
 #y and gamma same size
 #returns std dev of each row, weighted by gammas
-def weighted_std(y, gamma):
-    mu = weighted_mean(y,gamma);
-    mu_tiled = tile(mu, (1,y.shape[1])); 
-    wstd = sqrt(sum((gamma * (y-mu_tiled)**2), axis=1) / sum(gamma, axis=1));
-    wstd = wstd.reshape(wstd.shape[0],1);    
+def weighted_std(y, gamma, mu=None):
+    if(mu is None):
+        mu = weighted_mean(y,gamma);
+
+    wstd = np.sqrt(np.sum((gamma * (y-mu)**2), axis=1) / np.sum(gamma, axis=1));
+    wstd = wstd.reshape((wstd.shape[0],1));
     return wstd
