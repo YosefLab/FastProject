24731d8072716569530523330287db104f07c7bd
David DeTomaso <davedeto@gmail.com>
2015-12-08 11:40:45 -0800

    Bump to 0.9.2
    Signed-off-by: David DeTomaso <davedeto@gmail.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "FastProject",
-	version = "0.9.1",
+	version = "0.9.2",
 	packages = find_packages(),
 
 	entry_points = { 'console_scripts': ['FastProject = FastProject.__main__:entry']},
