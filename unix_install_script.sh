a068bb287698a92ceeca9d32610e517589075753
David DeTomaso <davedeto@gmail.com>
2015-12-31 19:29:36 -0800

    More changes to install script.
    Signed-off-by: David DeTomaso <davedeto@gmail.com>

--- unix_install_script.sh
+++ unix_install_script.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
 #Install virtualenv if not installed
 pip install virtualenv --user
+
+# For some reason, the user directory might not be on the path.
+# Add it there anyways, just in case
+export PATH=$PATH:$HOME/Library/Python/2.7/bin/
 
 #Create virtualenv for FastProject
 virtualenv FastProject
@@ -19,11 +24,13 @@ echo "" >> ~/.bash_profile
 echo "# Added by FastProject Installer" >> ~/.bash_profile
 echo "export PATH=\$PATH:$(pwd)" >> ~/.bash_profile
 
-#Re-source bash_profile
-source ~/.bash_profile
-
 #Deactivate virtualenv
+echo "Deactivate virtualenv"
 deactivate
+
+#Re-source bash_profile
+echo "Re-loading profile"
+source ~/.bash_profile
 
 #Backup to original directory
 cd ..
