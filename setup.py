a9541357327f95d5de58f013bdaae5efc317906a
David DeTomaso <davedeto@gmail.com>
2016-02-12 14:34:24 -0800

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "FastProject",
-	version = "0.9.2",
+	version = "1.0.0",
 	packages = find_packages(),
 
 	entry_points = { 'console_scripts': ['fastproject = FastProject.__main__:entry']},
