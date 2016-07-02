29ebf1fea61d3018f1aa88f8ca8c8f51faa7a5ff
David DeTomaso <davedeto@gmail.com>
2016-07-01 23:39:55 -0700

    Some tests and some test files.
    Signed-off-by: David DeTomaso <davedeto@gmail.com>

--- FastProject/Tests/full_battery.py
+++ FastProject/Tests/full_battery.py
@@ -1,8 +1,11 @@
+from __future__ import print_function, division;
 import os;
 from collections import namedtuple;
 from ..Pipelines import Analysis;
 from ..CLI import loadFilesFromDisk, createOutputDirectories, saveResultstoDisk;
+
 this_directory = os.path.dirname(os.path.abspath(__file__));
+TEST_FILE_DIR = os.path.join(this_directory, "TestFiles");
 
 
 def get_default_args():
@@ -15,10 +18,12 @@ def get_default_args():
                                        "all_sigs", "debug", "lean",
                                        "sig_norm_method", "sig_score_method"]);
 
-    args.data_file = "";
+    # Input Data Files
+    args.data_file = os.path.join(TEST_FILE_DIR, "smallData.txt");
+    args.signatures = [os.path.join(TEST_FILE_DIR, "sigsSmall.gmt")];
+    args.precomputed = [os.path.join(TEST_FILE_DIR, "precomputed_sigs.txt")];
+
     args.housekeeping = "";
-    args.signatures = [];
-    args.precomputed = [];
     args.output = "";
     args.nofilter = False;
     args.nomodel = False;
@@ -38,14 +43,33 @@ def get_default_args():
     return args;
 
 
-def run_simple():
+def test(func):
 
-    args = get_default_args();
+    def test_func():
+        args = func()
+        try:
+            run_test(args);
+        except:
+            with open('errors.log', 'a') as fout:
+                fout.write("Errors Found During Test: " + func.__name__ + "\n");
+                print("Errors Found During Test: " + func.__name__ + "\n");
+                fout.write("  ARG DUMP:\n");
+                for key in dir(args):
+                    if not key.startswith("_"):
+                        fout.write("    " + key + ": " + str(getattr(args, key)) + "\n");
+                import traceback
+                fout.write("  EXCEPTION:")
+                for line in traceback.format_exc().splitlines():
+                    fout.write("    " + line + "\n");
+        else:
+            with open('errors.log', 'a') as fout:
+                fout.write("Test Passed: " + func.__name__ + "\n");
+                print("Test Passed: " + func.__name__ + "\n");
 
-    # Input Data Files
-    args.data_file = os.path.join(this_directory, "TestFiles", "smallData.txt");
-    args.signatures = [os.path.join(this_directory, "TestFiles", "sigsSmall.txt")];
-    args.precomputed = [os.path.join(this_directory, "TestFiles", "precomputed_sigs.txt")];
+    return test_func;
+
+
+def run_test(args):
 
     (expressionMatrix, signatures, precomputed_signatures,
      housekeeping_genes, input_projections,
@@ -69,3 +93,291 @@ def run_simple():
         else:
             break;
         time.sleep(0.1);
+
+
+@test
+def test_simple():
+    args = get_default_args();
+
+    return args;
+
+
+@test
+def test_hk():
+    args = get_default_args();
+
+    args.housekeeping = os.path.join(TEST_FILE_DIR, "housekeeping.txt");
+
+    return args;
+
+
+@test
+def test_text_sigs():
+    args = get_default_args();
+
+    args.signatures = [os.path.join(TEST_FILE_DIR, "sigsA.txt")];
+
+    return args;
+
+
+@test
+def test_text_sigsB():
+    args = get_default_args();
+
+    args.signatures = [os.path.join(TEST_FILE_DIR, "sigsB.txt")];
+
+    return args;
+
+
+@test
+def test_multi_sigs():
+    args = get_default_args();
+
+    args.signatures = [os.path.join(TEST_FILE_DIR, "sigsA.txt"),
+                       os.path.join(TEST_FILE_DIR, "sigsSmall.gmt")];
+
+    return args;
+
+
+@test
+def test_no_precomputed():
+    args = get_default_args();
+
+    args.precomputed = [];
+
+    return args;
+
+
+@test
+def test_no_sigs():
+    args = get_default_args();
+
+    args.signatures = [];
+
+    return args;
+
+
+@test
+def test_qc():
+    args = get_default_args();
+
+    args.qc = True
+
+    return args;
+
+
+@test
+def test_nofilter():
+    args = get_default_args();
+
+    args.nofilter = True;
+
+    return args;
+
+
+@test
+def test_nomodel():
+    args = get_default_args();
+
+    args.nomodel = True;
+
+    return args;
+
+
+@test
+def test_pca_filter():
+    args = get_default_args();
+
+    args.pca_filter = True
+
+    return args;
+
+
+@test
+def test_weights():
+    args = get_default_args();
+
+    args.weights = os.path.join(TEST_FILE_DIR, "weights.txt");
+
+    return args;
+
+
+@test
+def test_projections():
+    args = get_default_args();
+
+    args.projections = ["input_projection_good.txt"];
+
+    return args;
+
+
+@test
+def test_many_projections():
+    args = get_default_args();
+
+    args.projections = ["input_projection_good.txt",
+        "input_projection_RFormat_ok.txt",
+        "input_projection_header_ok.txt",
+        "input_projection_header_ok2.txt"];
+
+    return args;
+
+
+@test
+def test_threshold():
+    args = get_default_args();
+
+    args.threshold = 10;
+
+    return args;
+
+
+@test
+def test_all_sigs():
+    args = get_default_args();
+
+    args.all_sigs = True;
+
+    return args;
+
+
+@test
+def test_debug():
+    args = get_default_args();
+
+    args.debug = True;
+
+    return args;
+
+
+@test
+def test_lean():
+    args = get_default_args();
+
+    args.lean = True;
+
+    return args;
+
+
+@test
+def test_sig_norm_method_none():
+    args = get_default_args();
+
+    args.sig_norm_method = "none";
+
+    return args;
+
+
+@test
+def test_sig_norm_method_znorm_columns():
+    args = get_default_args();
+
+    args.sig_norm_method = "znorm_columns";
+
+    return args;
+
+
+@test
+def test_sig_norm_method_znorm_rows():
+    args = get_default_args();
+
+    args.sig_norm_method = "znorm_rows";
+
+    return args;
+
+
+@test
+def test_sig_norm_method_znorm_rows_then_columns():
+    args = get_default_args();
+
+    args.sig_norm_method = "znorm_rows_then_columns";
+
+    return args;
+
+
+@test
+def test_sig_norm_method_rank_norm_columns():
+    args = get_default_args();
+
+    args.sig_norm_method = "rank_norm_columns";
+
+    return args;
+
+
+@test
+def test_sig_score_method_naive():
+    args = get_default_args();
+
+    args.sig_score_method = "naive";
+
+    return args;
+
+
+@test
+def test_sig_score_method_weighted_avg():
+    args = get_default_args();
+
+    args.sig_score_method = "weighted_avg";
+
+    return args;
+
+
+@test
+def test_sig_score_method_imputed():
+    args = get_default_args();
+
+    args.sig_score_method = "imputed";
+
+    return args;
+
+
+@test
+def test_sig_score_method_only_nonzero():
+    args = get_default_args();
+
+    args.sig_score_method = "only_nonzero";
+
+    return args;
+
+
+@test
+def test_output():
+    args = get_default_args();
+
+    args.output = "outfolder";
+
+    return args;
+
+
+def run_all():
+
+    test_simple();
+    test_hk();
+    test_text_sigs();
+    test_text_sigsB();
+    test_multi_sigs();
+    test_no_precomputed();
+    test_no_sigs();
+    test_qc();
+    test_nofilter();
+    test_nomodel();
+    test_pca_filter();
+    test_weights();
+    test_projections();
+    test_many_projections();
+    test_threshold();
+    test_all_sigs();
+    test_debug();
+    test_lean();
+    test_sig_norm_method_none();
+    test_sig_norm_method_znorm_columns();
+    test_sig_norm_method_znorm_rows();
+    test_sig_norm_method_znorm_rows_then_columns();
+    test_sig_norm_method_rank_norm_columns();
+    test_sig_score_method_naive();
+    test_sig_score_method_weighted_avg();
+    test_sig_score_method_imputed();
+    test_sig_score_method_only_nonzero();
+    test_output();
+
+
